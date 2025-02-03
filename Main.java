da2096d82fe0f3a5d602e777b752e3adee9199be
AzyPy <carlosgabriel.9d@gmail.com>
2025-02-03 14:55:26 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import java.util.Scanner;
 
 public class Main {
@@ -28,4 +29,36 @@ public class Main {
             System.out.print(" ");
         }
     }
+=======
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int digit = Integer.parseInt(scanner.nextLine());
+        christmasTree(digit);
+    }
+    public static void christmasTree(int size) {
+        for (int i = 1; i <= size; i++) {
+            printSpaces(size - i);
+            printStars((i + i) - 1);
+            System.out.println();
+        }
+        for (int i = 0; i < 2; i++) {
+            printSpaces(size - 2);
+            printStars(3);
+            System.out.println();
+        }
+    }
+    public static void printStars(int star) {
+        for (int i = 1; i <= star; i++) {
+            System.out.print("*");
+        }
+    }
+    public static void printSpaces(int number) {
+        for (int i = 1; i <= number; i++) {
+            System.out.print(" ");
+        }
+    }
+>>>>>>> Stashed changes
 }
